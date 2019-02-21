a716bfceb898430f25b0ed7626ec08221316943a
IDEALSCORP\m.pogidaev <maxim.pogidaev@idealscorp.com>
2019-02-21 10:22:57 +0200

    change auth btn

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -45,6 +45,7 @@ router.post("/login", async (req, res) => {
 // @desc    Get credentials
 // @access  Public
 router.get("/cred", (req, res) => {
+  console.log(userScope.join(" "));
   res.json({ client_id: userClientId, scope: userScope.join(" ") });
 });
 
