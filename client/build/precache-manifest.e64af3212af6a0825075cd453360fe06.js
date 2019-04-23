67dc73101c322d919efff7e1b5d267dd78aa16e6
IDEALSCORP\m.pogidaev <maxim.pogidaev@idealscorp.com>
2019-04-23 14:15:45 +0300

    v5 test

---
+++ client/build/precache-manifest.e64af3212af6a0825075cd453360fe06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "557336d4deab6dcb67e3bd72f34fa92e",
+    "url": "/static/media/spinner.557336d4.gif"
+  },
+  {
+    "revision": "e2f13e953cf94e69088b599f60b0dbb7",
+    "url": "/static/media/homePage.e2f13e95.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "31174a55dbace326b5f0",
+    "url": "/static/js/main.aa8c6419.chunk.js"
+  },
+  {
+    "revision": "6d77f963fcc8a5591054",
+    "url": "/static/js/2.64c0bc20.chunk.js"
+  },
+  {
+    "revision": "31174a55dbace326b5f0",
+    "url": "/static/css/main.e4c70c2b.chunk.css"
+  },
+  {
+    "revision": "8a21b4365c94a086d03d7b7d606d9770",
+    "url": "/index.html"
+  }
+];
