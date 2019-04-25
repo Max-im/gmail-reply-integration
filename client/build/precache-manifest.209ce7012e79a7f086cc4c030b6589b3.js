ffc3285e189e5512b196e6906590b85db532cc49
IDEALSCORP\m.pogidaev <maxim.pogidaev@idealscorp.com>
2019-04-25 12:08:37 +0300

    rebuild frontend

---
+++ client/build/precache-manifest.209ce7012e79a7f086cc4c030b6589b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb2fffe685407c1cd1ce839daa806c64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c902a1269a2e84e03cf",
+    "url": "/static/css/main.aedab2f6.chunk.css"
+  },
+  {
+    "revision": "21a8a6e675955f2af6d7",
+    "url": "/static/js/2.ca76169c.chunk.js"
+  },
+  {
+    "revision": "6c902a1269a2e84e03cf",
+    "url": "/static/js/main.e6e9e0dc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e2f13e953cf94e69088b599f60b0dbb7",
+    "url": "/static/media/homePage.e2f13e95.png"
+  },
+  {
+    "revision": "557336d4deab6dcb67e3bd72f34fa92e",
+    "url": "/static/media/spinner.557336d4.gif"
+  }
+]);
